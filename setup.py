116a251bd0ec23af3a347a72a3cccf2e50d07662
Tyler Garner <garnertb@gmail.com>
2015-07-07 12:02:45 -0400

    Update the build to MapStory/MapLoom/#369cb26df8.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-maploom',
-    version='0.0.1@2015-07-06.14:04:19.d13124aa2d',
+    version='0.0.1@2015-07-07.12:01:26.369cb26df8',
     author='LMN Solutions',
     author_email='geoshape.org@gmail.com',
     url='https://github.com/ROGUE-JCTD/django-maploom',
