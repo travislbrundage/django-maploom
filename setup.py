1db64ad5d733c7f67c18347943c2caa96957d3c7
Ami Rahav <arahav@boundlessgeo.com>
2017-02-13 08:07:31 -0500

    Jenkins bumped version to 1.5.42

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-exchange-maploom',
-    version='1.5.41',
+    version='1.5.42',
     author='GeoNode Development Team - Prominent Edge, Terranodo, Boundless Spatial',
     author_email='exchange@boundlessgeo.com',
     url='https://github.com/boundlessgeo/django-exchange-maploom',
